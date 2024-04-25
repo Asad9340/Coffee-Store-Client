b4009c26606de4805eab73bb258dd72f7246e55c
Asad9340 <asadulimran1999@gmail.com>
2024-04-25 12:31:18 +0600

    added some changes

--- src/Pages/AddCoffee/AddCoffee.jsx
+++ src/Pages/AddCoffee/AddCoffee.jsx
@@ -54,7 +54,7 @@ function AddCoffee() {
               <input
                 type="text"
                 name="name"
-                placeholder="Enter coffee name"
+                placeholder="Enter a coffee name"
                 className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
               />
             </div>
